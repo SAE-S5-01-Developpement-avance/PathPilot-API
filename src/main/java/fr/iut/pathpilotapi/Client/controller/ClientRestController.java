f4ef771941143404ddc5368a012cf39e1a0a70e1
Florian Fabre <florian.97118@gmail.com>
2024-11-27 13:42:34 +0100

    feat: added env variable

--- src/main/java/fr/iut/pathpilotapi/Client/controller/ClientRestController.java
+++ src/main/java/fr/iut/pathpilotapi/Client/controller/ClientRestController.java
@@ -1,4 +1,12 @@
+/*
+ * ClientRestController.java                                  18 oct. 2024
+ * IUT de Rodez, no author rights
+ */
+
 package fr.iut.pathpilotapi.Client.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("${api.base.url}")
 public class ClientRestController {
 }
