7c650bf01b7b017a87c53d0fcee0c5ede8841271
quentinformatique <quentin161204@gmail.com>
2024-12-09 17:42:45 +0100

    feat: update Route model

--- src/main/java/fr/iut/pathpilotapi/routes/Route.java
+++ src/main/java/fr/iut/pathpilotapi/routes/Route.java
@@ -5,9 +5,15 @@
 
 package fr.iut.pathpilotapi.routes;
 
-import fr.iut.pathpilotapi.client.Client;
+import com.mongodb.client.model.geojson.Position;
+import fr.iut.pathpilotapi.routes.dto.ClientDTO;
+import fr.iut.pathpilotapi.routes.dto.SalesmanDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +24,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import java.util.Date;
 import java.util.List;
 
-import static fr.iut.pathpilotapi.Constants.MAX_LENGTH;
-
 @Getter
 @Setter
 @RequiredArgsConstructor
@@ -27,18 +31,28 @@ import static fr.iut.pathpilotapi.Constants.MAX_LENGTH;
 @Schema(description = "Route entity representing a route to follow for a salesman")
 public class Route {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "Unique identifier of the route", example = "1")
+    private Integer _id;
+
+    @NotNull
+    @Schema(description = "the salesman who owns the route")
+    private SalesmanDTO salesman;
+
     @NotEmpty
+    @NotNull
     @Size(max = 8)
-    @Schema(description = "List og the clients to visit in the route", example = "[CLIENT_ID1, CLIENT_ID2, CLIENT_ID3]")
-    private List<String> idClientsToVisit;
+    @Schema(description = "List of the clients to visit in the route")
+    private List<@NotNull ClientDTO> clients_schedule;
 
     @Schema(description = "Start date of the route", example = "2024-12-06T00:00:00.000Z")
     private Date startDate;
 
-    @Schema(description = "Curent position of the salesman", example = "{type: 'Point', coordinates: [48.8566, 2.3522]}")
-    private GeoJson salesManCurrentPosition;
-
     @Size(max = 8)
-    @Schema(description = "List of the clients already visited", example = "[CLIENT_ID1, CLIENT_ID2, CLIENT_ID3]")
-    private List<String> idClientsVisited;
+    @Schema(description = "List of the clients already visited")
+    private List<@NotNull ClientDTO> clients_visited;
+
+    @Schema(description = "Curent position of the salesman", example = "{type: 'Point', coordinates: [48.8566, 2.3522]}")
+    private Position salesManCurrentPosition;
 }
