8f62367419249ff8d1037fe798054ffe89507c27
François SP <francois-sp@gmx.fr>
2024-12-13 10:05:24 +0100

    feat: add factory method for PositionDTO from Salesman
    - Introduced a static method to create PositionDTO from a Salesman object.
    - Enhanced the functionality of PositionDTO for better integration.

--- src/main/java/fr/iut/pathpilotapi/routes/dto/PositionDTO.java
+++ src/main/java/fr/iut/pathpilotapi/routes/dto/PositionDTO.java
@@ -5,6 +5,7 @@
 
 package fr.iut.pathpilotapi.routes.dto;
 
+import fr.iut.pathpilotapi.salesman.Salesman;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
@@ -38,6 +39,10 @@ public class PositionDTO {
     public PositionDTO(double longitude, double latitude) {
         this.longitude = longitude;
         this.latitude = latitude;
+    }
+
+    public static PositionDTO createFromSalesman(Salesman salesman) {
+        return new PositionDTO(salesman.getLongHomeAddress(), salesman.getLatHomeAddress());
     }
 
     @Override
