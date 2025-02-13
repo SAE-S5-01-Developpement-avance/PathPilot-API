cb41b8b4a24e4fd379aea51282b9fc2f99987478
François SP <francois-sp@gmx.fr>
2025-02-13 17:19:50 +0100

    feat: start BruteForceThread
    - Added logic to compute all possible client paths.
    - Introduced sublist processing for better performance.
    - Implemented methods to retrieve distances for paths.
    - Created a record type for managing possible paths.

--- src/main/java/fr/iut/pathpilotapi/algorithm/BruteForceThread.java
+++ src/main/java/fr/iut/pathpilotapi/algorithm/BruteForceThread.java
@@ -5,13 +5,51 @@
 
 package fr.iut.pathpilotapi.algorithm;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 /**
  * @author François de Saint Palais
  */
-public class BruteForceThread extends Algorithm {
+public class BruteForceThread extends BruteForce {
+
+    public static final int NB_ELEMENT_IN_SUBLIST = 100;
 
     @Override
     public void computeBestPath() {
-        //TODO implement
+        // List of every number between 1 and the number of clients (distances.size() - 1)
+        List<Integer> clientsIndex = IntStream.range(1, distances.size()).boxed().toList();
+
+        List<PossiblePath> allCombinaisons = getCombinaison(clientsIndex);
+        int subListCount = 1 + allCombinaisons.size() / NB_ELEMENT_IN_SUBLIST;
+        List<List<PossiblePath>> subList = new ArrayList<>(subListCount);
+
+        for (int i = 0; i < subListCount; i++) {
+            int end = Math.min(i + NB_ELEMENT_IN_SUBLIST, allCombinaisons.size());
+            subList.add(allCombinaisons.subList(i, end));
+        }
+
+        // TODO lancer un Thread pour chaque subList et récupérer le résultat
+    }
+
+    private List<PossiblePath> getCombinaison(List<Integer> list) {
+        return super.getCombinaisons(list).stream()
+                .map(PossiblePath::new)
+                .toList();
+    }
+
+
+    public Double getCompleteDistance(PossiblePath bestClientPath) {
+        return super.getCompleteDistance(bestClientPath.path());
+    }
+
+    public Double getDistance(PossiblePath bestClientPath) {
+        return super.getDistance(bestClientPath.path());
+    }
+
+    record PossiblePath(List<Integer> path) {
     }
 }
